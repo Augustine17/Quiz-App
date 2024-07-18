f6a4e8ca983b8b5c5561a7ed30aa0a992c800f7b
Augustine17 <augmen173@gmail.com>
2024-07-18 21:10:01 +0530

    change

--- src/components/QuestionSection.jsx
+++ src/components/QuestionSection.jsx
@@ -24,7 +24,7 @@ export default function QuestionSection({
         onChange={(e) => handleChange(e, currentQuestionIndex)}
       >
         {questions[currentQuestionIndex].choices.map((choice, i) => (
-          <div key={i} className="flex gap-2 cursor-pointer mb-2">
+          <div key={i} className="flex gap-2 cursor-pointer mb-2 items-center">
             <input
               type="radio"
               id={`choice-${i}`}
